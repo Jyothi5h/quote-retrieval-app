dc275af066d5c40044f298d49e58032e1ed9c575
Jyothish <163384215+Jyothi5h@users.noreply.github.com>
2025-06-10 15:33:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from sentence_transformers import SentenceTransformer
 
 # Load model and data
 model_path = os.path.join(os.path.dirname(__file__), "fine_tuned_bge_quote_model")
-model = SentenceTransformer(model_path, device='cpu')
+model = SentenceTransformer(model_path)
 df = pd.read_csv("quote_metadata.csv")
 corpus = df['quote'].tolist()
 index = faiss.read_index("quotes_index.faiss")
